60ffb916f845e2ba5acbe3596d58691744467dfd
marko-polo-cheno <37851975+marko-polo-cheno@users.noreply.github.com>
2021-05-20 21:07:29 -0400

    undo settings

--- commands/suggest.ts
+++ commands/suggest.ts
@@ -4,4 +4,7 @@ import Discord from 'discord.js';
 export const suggestCmd = async (message: Discord.Message, command: string, args: string[]) => {
   message.channel.send('yay');
   message.channel.send('test suggestion was: ' + args.toString());
+
+  // ack suggestion was taken
+  // save suggestion into DB
 };
