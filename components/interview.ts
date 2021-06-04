5c91c02339aa10c085c605e23f1102fb4691bb8e
alex <alexz2002429@gmail.com>
2021-06-04 13:55:06 -0400

    add link verify and parse

--- components/interview.ts
+++ components/interview.ts
@@ -1,15 +1,20 @@
 import { openDB } from './db';
 import Discord from 'discord.js';
-import extractDomain from 'extract-domain';
 
-interface Interviewer{
+interface Interviewer {
   UserId: number;
   Link: string;
 }
 
-function verifyLink(link: string){
-  const domain = extractDomain(link);
-  console.log("test"+domain);
+function parseLink(link: string) {
+  if (link.includes('calendly.com') || link.includes('x.ai')) {
+    if (!link.startsWith('http')) {
+      link = 'https://' + link;
+    }
+    return link;
+  } else {
+    return undefined;
+  }
 }
 
 export async function handleInterview(message: Discord.Message, args: string[], client: Discord.Client) {
@@ -22,8 +27,7 @@ export async function handleInterview(message: Discord.Message, args: string[], 
       listInterviewers(message, args, client);
       break;
     default:
-
-    break;
+      break;
   }
 }
 
@@ -36,35 +40,39 @@ export async function addInterviewer(message: Discord.Message, args: string[]) {
     message.channel.send('Missing arguments: ```Usage: .interviewer [name] [calendar-link]```');
     return;
   }
-  verifyLink(link);
+  const parsedLink = parseLink(link);
+  if (!parsedLink) {
+    message.channel.send(`Hmmm... I don't seem to recognize your meeting link. Be sure to use calendly or x.ai.`);
+    return;
+  }
   const res = await db.get('SELECT * FROM Interviewers WHERE UserID = ?', id);
   if (res == undefined) {
-    db.run('INSERT INTO Interviewers (UserId, Link) VALUES(? , ?)', id, link);
+    db.run('INSERT INTO Interviewers (UserId, Link) VALUES(? , ?)', id, parsedLink);
     message.channel.send(`<@${id}>, your info has been added. Thanks for signing up to help out! :codeyLove:`);
   } else {
-    db.run('UPDATE Interviewers SET Link = ? WHERE UserID = ?',link, id);
+    db.run('UPDATE Interviewers SET Link = ? WHERE UserID = ?', parsedLink, id);
     message.channel.send(`<@${id}>, your info has been changed.`);
   }
 }
 
 export async function listInterviewers(message: Discord.Message, args: string[], client: Discord.Client) {
   const db = await openDB();
-  const res = await db.all('SELECT * FROM Interviewers');
+  const res = shuffleArray(await db.all('SELECT * FROM Interviewers'));
   console.log(res);
-  let outEmbed = new Discord.MessageEmbed()
-  .setColor('#0099ff')
-  .setTitle('Interviewers');
-  for (const rows of res){
-    console.log(rows.UserId.toString())
-    outEmbed = outEmbed.addField((await client.users.fetch(rows['UserId'].toString())).tag, rows['Link'], true);
+  let outEmbed = new Discord.MessageEmbed().setColor('#0099ff').setTitle('Interviewers');
+  for (const rows of res) {
+    console.log(rows.UserId.toString());
+    outEmbed = outEmbed.addField('\u200B', (await client.users.fetch(rows['UserId'].toString())).tag, true);
+    outEmbed = outEmbed.addField('\u200B', '\u200B', true);
+    outEmbed = outEmbed.addField('\u200B', rows['Link'], true);
   }
   await message.channel.send(outEmbed);
 }
 
-function shuffleArray(array : Interviewer[]) {
+function shuffleArray(array: Interviewer[]) {
   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 }
