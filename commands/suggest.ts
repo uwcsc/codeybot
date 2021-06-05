6e3b96e0b0ec7e564dc3f949643ecc564ac497bf
marko-polo-cheno <37851975+marko-polo-cheno@users.noreply.github.com>
2021-06-04 21:17:50 -0400

    fixing bad db code for suggestions

--- commands/suggest.ts
+++ commands/suggest.ts
@@ -5,36 +5,22 @@ import { openDB, testDb } from '../components/db';
 export const suggestCmd = async (message: Discord.Message, args: string[]) => {
   try {
     // save suggestion into DB
-    const state = 'C'; // Create state = C
     const db = openDB();
+    const state = 'C'; // Create state = C
     var words = '';
     var word = '';
+    // Turn args into suggestion
     for (word in args) {
       words += word + ' ';
     }
 
     (await db).run(
-      'BEGIN TRANSACTION;' +
-        'CREATE TABLE IF NOT EXISTS suggestions (' +
-        '    suggestion_id IDENTITY(1,1) PRIMARY KEY,' +
-        '    suggestion_author VARCHAR(255) NOT NULL,' +
-        '    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,' +
-        '    suggestion VARCHAR(500) NOT NULL,' +
-        '    suggestion_state VARCHAR(1) NOT NULL' +
-        ');' +
-        'INSERT INTO suggestions(suggestion_author, suggestion, suggestion_state)' +
-        '    VALUES(' +
-        message.id +
-        ', ' +
-        words +
-        ', ' +
-        state +
-        ');' +
-        'COMMIT;'
+      'INSERT INTO suggestions(suggestion_author, suggestion, suggestion_state) ' + 'VALUES(?,?,?) ',
+      [message.id, words, state] + ');'
     );
 
     // confirm suggestion was taken
-    message.channel.send('Codey has recieved your suggestion: ' + args[0] + ' ' + args[1] + ' ' + args[2] + '... ');
+    message.channel.send('Codey has recieved your suggestion.');
   } catch (err) {
     // Error message
     message.channel.send('Sorry! There has been an error. Please try again later or let a mod know this happened.');
--- components/db.ts
+++ components/db.ts
@@ -1,22 +1,26 @@
-import sqlite3 = require('sqlite3')
-import { open, Database } from 'sqlite'
-import Discord from 'discord.js'
+import sqlite3 = require('sqlite3');
+import { open, Database } from 'sqlite';
+import Discord from 'discord.js';
 
-let db : Database | null = null;
+let db: Database | null = null;
 
-export async function openDB () {
-  if(db == null){
+export async function openDB() {
+  if (db == null) {
     db = await open({
       filename: './db/bot.db',
       driver: sqlite3.Database
-    })
-    await db.run('CREATE TABLE IF NOT EXISTS saved_data (msg_id INTEGER PRIMARY KEY,data TEXT NOT NULL);')
+    });
+    await db.run('CREATE TABLE IF NOT EXISTS saved_data (msg_id INTEGER PRIMARY KEY,data TEXT NOT NULL);');
+    await db.run(
+      'CREATE TABLE IF NOT EXISTS suggestions (suggestion_id INT AUTO_INCREMENT PRIMARY KEY,suggestion_author VARCHAR(255) NOT NULL,' +
+        'created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,suggestion VARCHAR(500) NOT NULL, suggestion_state VARCHAR(1) NOT NULL);'
+    );
   }
   return db;
 }
 
-export async function testDb(message: Discord.Message, command: string, args: string[]){
-  switch(command){
+export async function testDb(message: Discord.Message, command: string, args: string[]) {
+  switch (command) {
     case 'save':
       if (args.length < 1) {
         await message.channel.send('no args');
@@ -35,7 +39,7 @@ export async function testDb(message: Discord.Message, command: string, args: st
           .setTitle('Database Dump')
           .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
         const res = await db.all('SELECT * FROM saved_data');
-        for(const rows of res){
+        for (const rows of res) {
           console.log(rows['msg_id'], rows['data']);
           outEmbed = outEmbed.addField(rows['msg_id'], rows['data'], true);
           console.log(outEmbed);
@@ -65,4 +69,4 @@ export async function testDb(message: Discord.Message, command: string, args: st
   }
 }
 
-console.log('connected to db')
+console.log('connected to db');
