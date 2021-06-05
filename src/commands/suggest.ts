4e24778268b726cb8c6d827155047c38c9b86b5c
marko-polo-cheno <37851975+marko-polo-cheno@users.noreply.github.com>
2021-06-05 12:14:51 -0400

    column index out of range error

--- src/commands/suggest.ts
+++ src/commands/suggest.ts
@@ -3,24 +3,24 @@ import Discord from 'discord.js';
 import { openDB, testDb } from '../components/db';
 
 export const suggestCmd = async (message: Discord.Message, args: string[]) => {
-  try {
-    // save suggestion into DB
-    const db = openDB();
-    const state = 'C'; // Create state = C
-    var words = '';
-    var word = '';
-    // Turn args into suggestion
-    for (word in args) {
-      words += word + ' ';
-    }
+  const db = openDB();
+  const state = 'C'; // Create state = C
+  var words = '';
+  var word = '';
+  // Turn args into suggestion
+  for (word in args) {
+    words += word + ' ';
+  }
 
-    (await db).run('INSERT INTO suggestions(suggestion_author, suggestion, suggestion_state) VALUES(?,?,?);', [
+  try {
+    // Save suggestion into DB
+    (await db).run('INSERT INTO suggestions(suggestion_author, suggestion, suggestion_state) VALUES("?","?","?");', [
       message.id,
       words,
       state
     ]);
 
-    // confirm suggestion was taken
+    // Confirm suggestion was taken
     message.channel.send('Codey has recieved your suggestion.');
   } catch (err) {
     // Error message
