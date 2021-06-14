ffff874489acc69cb7c977e12710263b47eba7c1
alex <alexz2002429@gmail.com>
2021-06-13 23:49:45 -0400

    switch channel sends with reply

--- src/components/interview.ts
+++ src/components/interview.ts
@@ -75,9 +75,10 @@ async function help(message: Discord.Message): Promise<void> {
       { name: 'Get Interviewer List', value: '`.interviewer list (domain)`' },
       { name: 'Add Interviewer Domain', value: '`.interviewer domain [domain]`' },
       { name: 'Show Interviewer Profile', value: '`.interviewer profile`' },
-      { name: 'Clear Profile', value: '`.interviewer clear`' }
+      { name: 'Clear Profile', value: '`.interviewer clear`' },
+      { name: 'Available Domains', value: '`' + domainString() + '`' }
     );
-  await message.channel.send(response);
+  await message.reply(response);
 }
 
 async function addInterviewer(message: Discord.Message, args: string[]): Promise<void> {
@@ -94,7 +95,7 @@ async function addInterviewer(message: Discord.Message, args: string[]): Promise
   //parses link and checks for validity
   const parsedLink = parseLink(link);
   if (!parsedLink) {
-    message.channel.send(`Hmmm... I don't seem to recognize your meeting link. Be sure to use calendly or x.ai.`);
+    message.reply(`Hmmm... I don't seem to recognize your meeting link. Be sure to use calendly or x.ai.`);
     return;
   }
 
@@ -102,10 +103,10 @@ async function addInterviewer(message: Discord.Message, args: string[]): Promise
   const res = await db.get('SELECT * FROM interviewers WHERE user_id = ?', id);
   if (!res) {
     db.run('INSERT INTO interviewers (user_id, link) VALUES(? , ?)', id, parsedLink);
-    message.channel.send(`<@${id}>, your info has been added. Thanks for helping out! :codeyLove:`);
+    message.reply(`<@${id}>, your info has been added. Thanks for helping out! :codeyLove:`);
   } else {
     db.run('UPDATE interviewers SET link = ? WHERE user_id = ?', parsedLink, id);
-    message.channel.send(`<@${id}>, your info has been changed.`);
+    message.reply(`<@${id}>, your info has been changed.`);
   }
 }
 
@@ -119,7 +120,7 @@ async function listInterviewers(message: Discord.Message, client: Discord.Client
   if (!domain) {
     res = await db.all('SELECT * FROM interviewers');
   } else if (!(domain in available_domains)) {
-    await message.channel.send('Not a valid domain, valid domains are: ' + domainString());
+    await message.reply('Not a valid domain, valid domains are: ' + domainString());
     return;
   } else {
     res = await db.all(
@@ -139,7 +140,7 @@ async function listInterviewers(message: Discord.Message, client: Discord.Client
       '**' + (await client.users.fetch(rows['user_id'].toString())).tag + '** | [Calendar](' + rows['link'] + ')\n\n';
   }
   outEmbed.setDescription(listString);
-  await message.channel.send(outEmbed);
+  await message.reply(outEmbed);
 }
 
 async function clearProfile(message: Discord.Message): Promise<void> {
@@ -149,7 +150,7 @@ async function clearProfile(message: Discord.Message): Promise<void> {
   //clear user data from both tables
   await db.run('DELETE FROM interviewers WHERE user_id = ?', id);
   await db.run('DELETE FROM domains WHERE user_id = ?', id);
-  await message.channel.send('Your interviewer data has been cleared!');
+  await message.reply('Your interviewer data has been cleared!');
 }
 
 async function showProfile(message: Discord.Message): Promise<void> {
@@ -159,7 +160,7 @@ async function showProfile(message: Discord.Message): Promise<void> {
   //check if user signed up to be interviewer
   const interviewer = await db.get('SELECT * FROM interviewers WHERE user_id = ?', id);
   if (!interviewer) {
-    await message.channel.send("You don't seem to have signed up yet, run `.interviewer signup [link]`");
+    await message.reply("You don't seem to have signed up yet, run `.interviewer signup [link]`");
     return;
   }
 
@@ -179,7 +180,7 @@ async function showProfile(message: Discord.Message): Promise<void> {
   const outEmbed = new Discord.MessageEmbed().setColor('#0099ff').setTitle('Interviewer Profile');
   outEmbed.addField('**Link**', interviewer['link']);
   outEmbed.addField('**Domains**', domains === '' ? 'None' : domains);
-  await message.channel.send(outEmbed);
+  await message.reply(outEmbed);
 }
 
 async function addDomain(message: Discord.Message, args: string[]): Promise<void> {
@@ -189,7 +190,7 @@ async function addDomain(message: Discord.Message, args: string[]): Promise<void
   //check user signed up to be an interviewer
   const interviewer = await db.get('SELECT * FROM interviewers WHERE user_id = ?', id);
   if (!interviewer) {
-    await message.channel.send("You don't seem to have signed up yet, run ```.interviewer signup [link]```");
+    await message.reply("You don't seem to have signed up yet, run ```.interviewer signup [link]```");
     return;
   }
 
@@ -202,7 +203,7 @@ async function addDomain(message: Discord.Message, args: string[]): Promise<void
 
   //check if domain valid
   if (!(domain in available_domains)) {
-    await message.channel.send('Not a valid domain, valid domains are: ' + domainString());
+    await message.reply('Not a valid domain, valid domains are: ' + domainString());
     return;
   }
 
@@ -212,9 +213,9 @@ async function addDomain(message: Discord.Message, args: string[]): Promise<void
   //toggles on/off user's domain
   if (!res) {
     db.run('INSERT INTO domains (user_id, domain) VALUES(?, ?)', id, domain);
-    await message.channel.send('You have been successfully added to ' + available_domains[domain]);
+    await message.reply('You have been successfully added to ' + available_domains[domain]);
   } else {
     db.run('DELETE FROM domains WHERE user_id = ? AND domain = ?', id, domain);
-    await message.channel.send('You have been successfully removed from ' + available_domains[domain]);
+    await message.reply('You have been successfully removed from ' + available_domains[domain]);
   }
 }
