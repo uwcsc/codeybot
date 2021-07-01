cc87a484c4588d0e6147b7216ea8c231d09793db
marko-polo-cheno <37851975+marko-polo-cheno@users.noreply.github.com>
2021-07-01 19:26:04 -0400

    new env var for diff channel

--- src/components/cron.ts
+++ src/components/cron.ts
@@ -2,13 +2,15 @@ import { updateSuggestionCron } from './suggestions';
 import { TextChannel } from 'discord.js';
 import { CommandoClient } from 'discord.js-commando';
 
+const NOTIF_CHANNEL_ID_OLD: string = process.env.NOTIF_CHANNEL_ID_OLD || '.';
+
 const CronJob = require('cron').CronJob;
 
 // Checks for new suggestions every 10 mins
 export const suggestionCronJob = new CronJob('0 */10 * * * *', async function (client: CommandoClient) {
   const isSuggestionsChanged = await updateSuggestionCron();
   if (isSuggestionsChanged) {
-    const messageChannel = client.channels.cache.get(`701335585272627203`);
+    const messageChannel = client.channels.cache.get(NOTIF_CHANNEL_ID_OLD);
     if (messageChannel == undefined) {
       throw 'Bad Channel';
     } else if (messageChannel.type === 'text') {
