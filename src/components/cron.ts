f389a042b10e183b7ff03962f82dc43e151c1558
marko-polo-cheno <37851975+marko-polo-cheno@users.noreply.github.com>
2021-07-01 19:34:48 -0400

    1 min to track error

--- src/components/cron.ts
+++ src/components/cron.ts
@@ -7,7 +7,7 @@ const NOTIF_CHANNEL_ID_OLD: string = process.env.NOTIF_CHANNEL_ID_OLD || '.';
 const CronJob = require('cron').CronJob;
 
 // Checks for new suggestions every 10 mins
-export const suggestionCronJob = new CronJob('0 */10 * * * *', async function (client: CommandoClient) {
+export const suggestionCronJob = new CronJob('0 */1 * * * *', async function (client: CommandoClient) {
   const isSuggestionsChanged = await updateSuggestionCron();
   if (isSuggestionsChanged) {
     const messageChannel = client.channels.cache.get(NOTIF_CHANNEL_ID_OLD);
