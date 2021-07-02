e8cadb76f2d713da882dbfdeb8c185d63263cf47
marko-polo-cheno <37851975+marko-polo-cheno@users.noreply.github.com>
2021-07-01 20:04:15 -0400

    fixed cron not function issue ty Charles

--- src/bot.ts
+++ src/bot.ts
@@ -10,7 +10,7 @@ import path from 'path';
 import { openCommandoDB } from './components/db';
 import logger, { logError } from './components/logger';
 import { initEmojis } from './components/emojis';
-import { suggestionCronJob } from './components/cron';
+import { createSuggestionCron } from './components/cron';
 
 const NOTIF_CHANNEL_ID: string = process.env.NOTIF_CHANNEL_ID || '.';
 const BOT_TOKEN: string = process.env.BOT_TOKEN || '.';
@@ -40,7 +40,7 @@ export const startBot = async (): Promise<void> => {
     });
     const notif = (await client.channels.fetch(NOTIF_CHANNEL_ID)) as Discord.TextChannel;
     initEmojis(client);
-    suggestionCronJob.start();
+    createSuggestionCron(client).start();
     notif.send('Codey is up!');
   });
 
--- src/components/cron.ts
+++ src/components/cron.ts
@@ -6,17 +6,18 @@ const NOTIF_CHANNEL_ID_OLD: string = process.env.NOTIF_CHANNEL_ID_OLD || '.';
 
 const CronJob = require('cron').CronJob;
 
-// Checks for new suggestions every 10 mins
-export const suggestionCronJob = new CronJob('0 */1 * * * *', async function (client: CommandoClient) {
-  const isSuggestionsChanged = await updateSuggestionCron();
-  if (isSuggestionsChanged) {
-    const messageChannel = client.channels.cache.get(NOTIF_CHANNEL_ID_OLD);
-    if (messageChannel == undefined) {
-      throw 'Bad Channel';
-    } else if (messageChannel.type === 'text') {
-      (messageChannel as TextChannel).send(`New suggestions exist.`);
-    } else {
-      throw 'Bad Channel Type';
+// Checks for new suggestions every min
+export const createSuggestionCron = (client: CommandoClient) =>
+  new CronJob('0 */1 * * * *', async function () {
+    const isSuggestionsChanged = await updateSuggestionCron();
+    if (isSuggestionsChanged) {
+      const messageChannel = client.channels.cache.get(NOTIF_CHANNEL_ID_OLD);
+      if (messageChannel == undefined) {
+        throw 'Bad Channel';
+      } else if (messageChannel.type === 'text') {
+        (messageChannel as TextChannel).send(`New suggestions exist.`);
+      } else {
+        throw 'Bad Channel Type';
+      }
     }
-  }
-});
+  });
