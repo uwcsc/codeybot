dcd832a96375cdc82d60c9ccd42e6f5bcfda2e62
Mark Chen <37851975+marko-polo-cheno@users.noreply.github.com>
2021-07-21 19:06:19 -0400

    Suggestion state update (#46)
    * created suggestion update by id
    
    * created suggestion update by id
    
    * finished updateSugg
    
    * lint
    
    * lint
    
    * suggestUpdate addressed comments
    
    * fix more things
    
    * done sugg update

--- src/commands/suggestions/utils.ts
+++ src/commands/suggestions/utils.ts
@@ -1,4 +1,16 @@
+import _ from 'lodash';
 import { suggestionStatesReadable, getAvailableStatesString } from '../../components/suggestions';
+
+export const validateIDs = (value: string): boolean | string => {
+  // Split by white space
+  const values = value.split(' ').map((a) => Number(a));
+
+  // validate each id after first word
+  if (_.some(values, isNaN)) {
+    return `you entered an invalid id. Please enter numbers only.`;
+  }
+  return true;
+};
 
 export const validateState = (value: string): boolean | string => {
   // validate if this is one of the available states
