8da50048e1d51e04f211faef63d3719f0704f5a3
Mark Chen <markzchen1@gmail.com>
2021-11-15 01:45:39 -0500

    no dms

--- src/components/messageListener.ts
+++ src/components/messageListener.ts
@@ -1,8 +1,7 @@
 import { Message } from 'discord.js';
 import { applyBonusByUserId } from './coin';
 import { client } from '../bot';
-
-const DM = 'dm';
+import { TextChannel } from 'discord.js';
 
 export const messageListener = async (message: Message): Promise<void> => {
   if (!client.user) {
@@ -10,7 +9,7 @@ export const messageListener = async (message: Message): Promise<void> => {
   }
 
   // Ignore all messages created by the bot and dms from users
-  if (message.author.id != client.user.id && message.channel.type !== DM) {
+  if (message.author.id != client.user.id && message.channel instanceof TextChannel) {
     await applyBonusByUserId(message.author.id);
   }
 };
