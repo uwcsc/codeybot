90202140822955986697ee6942348492670912d0
Andrew Wang <anwang@hubspot.com>
2022-01-19 10:25:43 -0500

    temp fixes for event

--- src/bot.ts
+++ src/bot.ts
@@ -81,8 +81,6 @@ export const startBot = async (): Promise<void> => {
     notif.send('Codey is up!');
   });
 
-  client.on('message', messageListener);
-
   client.on('error', logError);
 
   client.login(BOT_TOKEN);
