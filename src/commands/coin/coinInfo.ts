88ae7f2c74787ae65185e030dc12a51fc7a6fe65
Charles Zhang <gzcharleszhang@hotmail.com>
2022-01-27 14:28:21 -0500

    changed activity bonus cooldown to 5 mins

--- src/commands/coin/coinInfo.ts
+++ src/commands/coin/coinInfo.ts
@@ -18,7 +18,7 @@ class CoinInfo extends BaseCommand {
     .setColor(EMBED_COLOUR)
     .setTitle('🪙   About Codey Coin   🪙')
     .setThumbnail('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/282/coin_1fa99.png') // Thumbnail, if needed?
-    .setDescription(`Codey Coin are rewarded for being active in CSC's events and discord!`)
+    .setDescription(`Codey coins are rewarded for being active in CSC's events and discord!`)
     .addFields(
       {
         name: '🪙   How Can I Obtain Codey Coins?',
@@ -26,7 +26,7 @@ class CoinInfo extends BaseCommand {
           • Participating in CSC events
           • Being active on Discord
           ---Daily bonus - your first message of the day on CSC's Discord will grant some Codey coins
-          ---Activity bonus - your first message of every minute on CSC's Discord will grant some Codey coins`
+          ---Activity bonus - your first message of every 5 minutes on CSC's Discord will grant some Codey coins`
       },
       {
         name: '🪙   What Can I Do With Codey Coins?',
