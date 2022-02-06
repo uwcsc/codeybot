cdd0fa8b31783a47aa40b8bf0434bf722b754715
Marcus Chan <marcus.chanwc@gmail.com>
2022-02-05 23:09:39 -0500

    added arguments to getResume command

--- src/commands/resumes/getResume.ts
+++ src/commands/resumes/getResume.ts
@@ -1,6 +1,7 @@
-import { Message } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
 import { CommandoClient, CommandoMessage } from 'discord.js-commando';
 import { BaseCommand } from '../../utils/commands';
+import { RESUME_EMBED_COLOUR } from '../../utils/embeds';
 
 class GetResumeCommand extends BaseCommand {
   constructor(client: CommandoClient) {
@@ -10,12 +11,40 @@ class GetResumeCommand extends BaseCommand {
       group: 'resumes',
       memberName: 'get-resume',
       description: 'Get an example resume!',
-      examples: []
+      examples: [
+        `${client.commandPrefix}resume web-dev 2`
+      ],
+      args: [
+        {
+          key: 'field',
+          prompt: 'The field of the resume (eg web-dev)',
+          type: 'string'
+        },
+        {
+          key: 'term',
+          prompt: 'The work-term the person with the resume will be in during their co-op (eg 3)',
+          type: 'integer'
+        }
+      ]
     });
   }
 
-  async onRun(message: CommandoMessage): Promise<Message> {
-    return message.reply(`This is a message for example resumes!`);
+  getResumeEmbed = (field: string, term: number) => new MessageEmbed()
+    .setColor(RESUME_EMBED_COLOUR)
+    .setTitle('Example Resume')
+    .setDescription(`Example resume for ${field} for work term ${term}`)
+    .addFields(
+        {
+            name: 'Resume Link',
+            value: 'https://google.com'
+        }
+    );
+    
+
+  async onRun(message: CommandoMessage, args: { field: string; term: number }): Promise<Message> {
+    return message.channel.send(
+      this.getResumeEmbed(args.field, args.term)
+    );
   }
 }
 
--- src/utils/embeds.ts
+++ src/utils/embeds.ts
@@ -1,1 +1,2 @@
 export const EMBED_COLOUR = '#0099ff';
+export const RESUME_EMBED_COLOUR = '#77bb00';
