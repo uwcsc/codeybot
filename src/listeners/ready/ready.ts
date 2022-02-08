181367da0da43c880c73d5a01b07740b0949e79e
alex <alexz2002429@gmail.com>
2022-02-08 12:41:17 -0500

    move ready message to ready listener

--- src/listeners/ready/ready.ts
+++ src/listeners/ready/ready.ts
@@ -1,8 +1,12 @@
 import type { ListenerOptions, Piece, PieceContext } from '@sapphire/framework';
 import { Listener, Store } from '@sapphire/framework';
 import { blue, gray, green, magenta, magentaBright, white, yellow } from 'colorette';
+import { vars } from '../../config';
+import Discord from 'discord.js';
 
 const dev = process.env.NODE_ENV !== 'production';
+
+const NOTIF_CHANNEL_ID = vars.NOTIF_CHANNEL_ID;
 
 export class ReadyListener extends Listener {
   private readonly style = dev ? yellow : blue;
@@ -14,12 +18,22 @@ export class ReadyListener extends Listener {
     });
   }
 
-  public run(): void {
+  run = (): void => {
     this.printBanner();
     this.printStoreDebugInformation();
-  }
+    this.sendReady();
+  };
 
-  private printBanner() {
+  sendReady = async (): Promise<void> => {
+    const { client, logger } = this.container;
+    logger.info({
+      event: 'init'
+    });
+    const notif = (await client.channels.fetch(NOTIF_CHANNEL_ID)) as Discord.TextChannel;
+    notif.send('Codey is up!');
+  };
+
+  printBanner = (): void => {
     const success = green('+');
 
     const llc = dev ? magentaBright : white;
@@ -39,18 +53,18 @@ ${line02} ${pad}[${success}] Gateway
 ${line03}${dev ? ` ${pad}${blc('<')}${llc('/')}${blc('>')} ${llc('DEVELOPMENT MODE')}` : ''}
 		`.trim()
     );
-  }
+  };
 
-  private printStoreDebugInformation() {
+  printStoreDebugInformation = (): void => {
     const { client, logger } = this.container;
     const stores = [...client.stores.values()];
     const last = stores.pop()!;
 
     for (const store of stores) logger.info(this.styleStore(store, false));
     logger.info(this.styleStore(last, true));
-  }
+  };
 
-  private styleStore(store: Store<Piece>, last: boolean) {
+  styleStore = (store: Store<Piece>, last: boolean): string => {
     return gray(`${last ? '└─' : '├─'} Loaded ${this.style(store.size.toString().padEnd(3, ' '))} ${store.name}.`);
-  }
+  };
 }
