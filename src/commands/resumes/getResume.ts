c24e122efabe0a1a8f8d6348ed86b7ee6db65865
Marcus Chan <marcus.chanwc@gmail.com>
2022-02-21 13:35:31 -0500

    moved getResumeEmbed back to getResume.ts

--- src/commands/resumes/getResume.ts
+++ src/commands/resumes/getResume.ts
@@ -1,9 +1,41 @@
 import { Message, MessageEmbed } from 'discord.js';
 import { CommandoClient, CommandoMessage } from 'discord.js-commando';
 import { BaseCommand } from '../../utils/commands';
-import { getResumeEmbed } from '../../utils/embeds';
 
 import {default as resumes} from '../resumes/resumes.json';
+
+import { availableDomains } from '../../components/interview';
+
+const RESUME_EMBED_COLOUR = '#77bb00';
+
+/*
+* Send embed for getting an example resume
+*/
+const getResumeEmbed = (domain: string, term: string, resumes: any): MessageEmbed => {
+  try {
+    // get link to resume (just first one in the "list" for now)
+    const resumeLink = resumes[domain][term][0].url;
+    const domainName = availableDomains[domain];
+
+    return new MessageEmbed()
+      .setColor(RESUME_EMBED_COLOUR)
+      .setTitle(`Example Resume For ${domainName}`)
+      .setDescription(`An example resume for ${domainName} for work term ${term}`)
+      .addFields({
+        name: 'Resume Link',
+        value: resumeLink
+      });
+  } catch (e) {
+    return new MessageEmbed()
+      .setColor(RESUME_EMBED_COLOUR)
+      .setTitle(`No resume found!`)
+      .setDescription(`Sorry, we could not find a resume in the category you are looking for. Please try again.`)
+      .addFields({
+        name: 'Sorry!',
+        value: 'No resumes exist for this combination of category and term.'
+      });
+  }
+};
 
 class GetResumeCommand extends BaseCommand {
   constructor(client: CommandoClient) {
@@ -34,7 +66,7 @@ class GetResumeCommand extends BaseCommand {
     });
   }
 
-  async onRun(message: CommandoMessage, args: { field: string; term: string }): Promise<Message> {
+  async onRun(message: CommandoMessage, args: { field: string; term: string; quality: string }): Promise<Message> {
     return message.channel.send(getResumeEmbed(args.field, args.term, resumes));
   }
 }
--- src/utils/embeds.ts
+++ src/utils/embeds.ts
@@ -2,12 +2,9 @@ import { Message, MessageEmbed, User, TextChannel } from 'discord.js';
 import { client } from '../bot';
 import { vars } from '../config';
 
-import { availableDomains } from '../components/interview';
-
 const NOTIF_CHANNEL_ID: string = vars.NOTIF_CHANNEL_ID;
 
 export const EMBED_COLOUR = '#0099ff';
-export const RESUME_EMBED_COLOUR = '#77bb00';
 
 /*
  * Send kick embed
@@ -28,33 +25,4 @@ export const sendKickEmbed = async (message: Message, user: User, reason = '', i
     });
   }
   await (client.channels.cache.get(NOTIF_CHANNEL_ID) as TextChannel).send(kickEmbed);
-};
-
-/*
-* Send embed for getting an example resume
-*/
-export const getResumeEmbed = (domain: string, term: string, resumes: any): MessageEmbed => {
-  try {
-    // get link to resume (just first one in the "list" for now)
-    const resumeLink = resumes[domain][term][0].url;
-    const domainName = availableDomains[domain];
-
-    return new MessageEmbed()
-      .setColor(RESUME_EMBED_COLOUR)
-      .setTitle(`Example Resume For ${availableDomains[domain]}`)
-      .setDescription(`An example resume for ${availableDomains[domain]} for work term ${term}`)
-      .addFields({
-        name: 'Resume Link',
-        value: resumeLink
-      });
-  } catch (e) {
-    return new MessageEmbed()
-      .setColor(RESUME_EMBED_COLOUR)
-      .setTitle(`Example Resume For ${availableDomains[domain]}`)
-      .setDescription(`Example resume for ${domain} for work term ${term}`)
-      .addFields({
-        name: 'Sorry!',
-        value: 'No resumes exist for this combination of category and term.'
-      });
-  }
 };
