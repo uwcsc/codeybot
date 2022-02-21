0f55c72cfe65bb0255bc52b008d0051230d7e915
Marcus Chan <marcus.chanwc@gmail.com>
2022-02-21 14:35:38 -0500

    fixed linting

--- src/commands/resumes/getResume.ts
+++ src/commands/resumes/getResume.ts
@@ -2,25 +2,30 @@ import { Message, MessageEmbed } from 'discord.js';
 import { CommandoClient, CommandoMessage } from 'discord.js-commando';
 import { BaseCommand } from '../../utils/commands';
 
-import yaml from 'js-yaml';
-import { readFileSync } from 'fs';
-
-import {default as resumes} from './resumes.json';
-
-import logger from '../../components/logger';
+import { default as resumes } from './resumes.json';
 
 import { availableDomains } from '../../components/interview';
 
 const RESUME_EMBED_COLOUR = '#77bb00';
 
+interface Resume {
+  url: string;
+  author: string;
+}
+
 /*
-* Send embed for getting an example resume
-*/
-const getResumeEmbed = (domain: string, term: string, quality: string, resumes: any): MessageEmbed => {
+ * Send embed for getting an example resume
+ */
+const getResumeEmbed = (
+  domain: string,
+  term: number,
+  quality: string,
+  resumes: { [key: string]: { [key: number]: { [key: string]: Resume[] } } }
+): MessageEmbed => {
   try {
     // get link to resume (just first one in the "list" for now)
-    logger.info(resumes);
     const resumeLink = resumes[domain][term][quality][0].url;
+    // get domain name
     const domainName = availableDomains[domain];
 
     return new MessageEmbed()
@@ -33,26 +38,30 @@ const getResumeEmbed = (domain: string, term: string, quality: string, resumes: 
       });
   } catch (e) {
     // if error, we return a list of all the possible categories
-    let categoryString: string = "";
+    let categoryString = '';
     for (const [key, name] of Object.entries(availableDomains)) {
-      categoryString += `⚙  ${key} - **${name}**\n`
+      categoryString += `⚙  ${key} - **${name}**\n`;
     }
     return new MessageEmbed()
       .setColor(RESUME_EMBED_COLOUR)
       .setTitle(`No resume found!`)
-      .setDescription(`Sorry, we could not find a resume in the category you are looking for. Please check the available categories down below and try again.`)
-      .addFields({
-        name: 'Fields',
-        value: categoryString,
-      },
-      {
-        name: 'Term',
-        value: '1, 2, 3, 4, 5 or 6',
-      },
-      {
-        name: 'Quality',
-        value: '✅ Good\n❌ Bad\n'
-      });
+      .setDescription(
+        `Sorry, we could not find a resume in the category you are looking for. Please check the available categories down below and try again.`
+      )
+      .addFields(
+        {
+          name: 'Fields',
+          value: categoryString
+        },
+        {
+          name: 'Term',
+          value: '1, 2, 3, 4, 5 or 6'
+        },
+        {
+          name: 'Quality',
+          value: '✅ Good\n❌ Bad\n'
+        }
+      );
   }
 };
 
@@ -85,7 +94,7 @@ class GetResumeCommand extends BaseCommand {
     });
   }
 
-  async onRun(message: CommandoMessage, args: { field: string; term: string; quality: string }): Promise<Message> {
+  async onRun(message: CommandoMessage, args: { field: string; term: number; quality: string }): Promise<Message> {
     // load resumes from json file
     // logger.info(resumes);
     return message.channel.send(getResumeEmbed(args.field, args.term, args.quality, resumes));
