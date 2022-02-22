d841c416c3f6c09a83657bb90543e47cec6ed6b0
Picowchew The Panda <58180935+Picowchew@users.noreply.github.com>
2022-02-22 15:46:26 -0500

    Migrated games to Sapphire (#172)
    * Migrated newer additions
    
    * Migrated games to Sapphire
    
    * Added comment and updated examples
    
    * Updated method of retrieving token

--- src/components/db.ts
+++ src/components/db.ts
@@ -101,20 +101,19 @@ const initUserCoinTable = async (db: Database): Promise<void> => {
 const initUserProfileTable = async (db: Database): Promise<void> => {
   await db.run(
     `
-        CREATE TABLE IF NOT EXISTS user_profile_table (
-            user_id VARCHAR(255) PRIMARY KEY NOT NULL,
-            about_me TEXT, 
-            birth_date TEXT,
-            preferred_name VARCHAR(32),
-            preferred_pronouns VARCHAR(16),
-            term VARCHAR(2),
-            year INTEGER,
-            last_updated TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
-            faculty VARCHAR(32),
-            program VARCHAR(32),
-            specialization VARCHAR(32)
-        )
-        `
+    CREATE TABLE IF NOT EXISTS user_profile_table (
+      user_id VARCHAR(255) PRIMARY KEY NOT NULL,
+      about_me TEXT,
+      birth_date TEXT,
+      preferred_name VARCHAR(32),
+      preferred_pronouns VARCHAR(16),
+      term VARCHAR(2),
+      year INTEGER,
+      last_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      major VARCHAR(16),
+      program VARCHAR(32)
+    )
+    `
   );
 };
 
