16637c636fb660db707a3938a550115e6dd130ac
Alluq <allen.lu@hotmail.ca>
2022-03-01 23:52:23 -0500

    Changed constructor to sapphire decorator for flipCoin

--- src/commands/fun/flipCoin.ts
+++ src/commands/fun/flipCoin.ts
@@ -1,19 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { Message } from 'discord.js';
-import { Args, Command} from '@sapphire/framework';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Args, Command, CommandOptions} from '@sapphire/framework';
 import { BOT_PREFIX } from '../../bot'
 
-class FlipCoinCommand extends Command{
-  constructor(context: Command.Context) {
-    super(context, {
+@ApplyOptions<CommandOptions>({
       name: 'flip-coin',
       aliases: ['fc', 'flip', 'flipcoin', 'coin-flip', 'coinflip'],
       fullCategory: ['fun'],
       description: 'Flip a coin! In making decisions, if it is not great, at least it is fair!',
       detailedDescription: `Example: ${BOT_PREFIX}flip-coin`
-    });
-  }
-
+})
+class FlipCoinCommand extends Command{
   async messageRun(message: Message): Promise<Message> {
     const onHeads = Math.random() < 0.5;
     return message.reply(`The coin landed on **${onHeads ? 'heads' : 'tails'}**!`);
