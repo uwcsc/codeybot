4127e0bc9089f8086e76bd65ff2989f48a1f55f1
mcpenguin <marcus.chanwc@gmail.com>
2022-06-06 20:10:08 -0400

    Added ping slash command

--- src/commands/ping/ping.ts
+++ src/commands/ping/ping.ts
@@ -1,21 +1,32 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command, CommandOptions, container } from '@sapphire/framework';
+import { Args, Command, container } from '@sapphire/framework';
 import type { Message } from 'discord.js';
+import { isMessageInstance } from '@sapphire/discord.js-utilities';
 
-@ApplyOptions<CommandOptions>({
-  aliases: ['pong'],
+import { vars } from '../../config';
+
+@ApplyOptions<Command.Options>({
+  aliases: ['ping'],
   description: 'ping pong',
   detailedDescription: `**Examples:**
-\`${container.botPrefix}ping\`
-\`${container.botPrefix}pong\``
-})
-export class PingCommand extends Command {
-  async messageRun(message: Message): Promise<Message> {
-    const { client } = container;
-    const msg = await message.channel.send('Ping?');
-    const content = `Pong from JavaScript! Bot Latency ${Math.round(client.ws.ping)}ms. API Latency ${
-      msg.createdTimestamp - message.createdTimestamp
-    }ms.`;
-    return msg.edit(content);
+    \`${container.botPrefix}ping\`
+    \`${container.botPrefix}pong\``,
+  chatInputCommand: {
+    register: true
   }
+})
+
+export class PingCommand extends Command {
+
+  public async chatInputRun(interaction: Command.ChatInputInteraction) {
+    const msg = await interaction.reply({ content: `Ping?`, ephemeral: true, fetchReply: true });
+    if (isMessageInstance(msg)) {
+      const diff = msg.createdTimestamp - interaction.createdTimestamp;
+      const ping = Math.round(this.container.client.ws.ping);
+
+      return interaction.editReply(`Pong 🏓! (Round trip took: ${diff}ms. Heartbeat: ${ping}ms.)`);
+    }
+    return interaction.editReply('Failed to retrieve ping :(');
+  }
+
 }
