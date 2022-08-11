905177d47282742824df197c49d8d162377bdfd8
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2022-08-11 17:34:18 -0400

    Fix undefined coin emoji in .coin info

--- src/commandDetails/coin/info.ts
+++ src/commandDetails/coin/info.ts
@@ -9,36 +9,34 @@ import {
 import { getCoinEmoji } from '../../components/emojis';
 import { EMBED_COLOUR } from '../../utils/embeds';
 
-// Get information about coin
-const COIN = getCoinEmoji();
-const infoEmbed = new MessageEmbed()
-  .setColor(EMBED_COLOUR)
-  .setTitle(`${COIN}   About Codey Coin   ${COIN}`)
-  .setThumbnail('https://cdn.discordapp.com/emojis/937096777180516453.webp')
-  .setDescription(`Codey coins are rewarded for being active in CSC's events and discord!`)
-  .addFields(
-    {
-      name: `${COIN}   How Can I Obtain Codey Coins?`,
-      value: `Earn Codey coins by:
-      • Participating in CSC events
-      • Being active on Discord
-      ---Daily bonus - your first message of the day on CSC's Discord will grant some Codey coins
-      ---Activity bonus - your first message of every 5 minutes on CSC's Discord will grant some Codey coins`
-    },
-    {
-      name: `${COIN}   What Can I Do With Codey Coins?`,
-      value: `Use Codey coins to:
-      • Play Casino games such as Blackjack
-      • Buy virtual CSC Swag in the server (more info to come!)`
-    }
-  );
-
 const coinInfoExecuteCommand: SapphireMessageExecuteType = async (
   _client,
   _messageFromUser,
   _args
 ): Promise<SapphireMessageResponse> => {
-  return infoEmbed;
+  // Get information about coin
+  const COIN = getCoinEmoji();
+  return new MessageEmbed()
+    .setColor(EMBED_COLOUR)
+    .setTitle(`${COIN}   About Codey Coin   ${COIN}`)
+    .setThumbnail('https://cdn.discordapp.com/emojis/937096777180516453.webp')
+    .setDescription(`Codey coins are rewarded for being active in CSC's events and discord!`)
+    .addFields(
+      {
+        name: `${COIN}   How Can I Obtain Codey Coins?`,
+        value: `Earn Codey coins by:
+          • Participating in CSC events
+          • Being active on Discord
+          ---Daily bonus - your first message of the day on CSC's Discord will grant some Codey coins
+          ---Activity bonus - your first message of every 5 minutes on CSC's Discord will grant some Codey coins`
+      },
+      {
+        name: `${COIN}   What Can I Do With Codey Coins?`,
+        value: `Use Codey coins to:
+          • Play Casino games such as Blackjack
+          • Buy virtual CSC Swag in the server (more info to come!)`
+      }
+    );
 };
 
 export const coinInfoCommandDetails: CodeyCommandDetails = {
