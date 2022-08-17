1ffafbad0f8c977dfddea52bf3bd3ce23507bfb7
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2022-08-17 12:35:00 -0400

    Create member.ts (#306)
    * Create member.ts
    
    * Move member logic to commandDetails
    
    * Create member.ts commandDetails
    
    * Implement suggested changes
    
    * Move remaining logic to commandDetails
    
    * Update member API endpoint

---
+++ src/commandDetails/miscellaneous/member.ts
@@ -0,0 +1,90 @@
+import { container } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+import fetch from 'node-fetch';
+import {
+  CodeyCommandDetails,
+  CodeyCommandOptionType,
+  CodeyCommandResponseType,
+  SapphireMessageExecuteType,
+  SapphireMessageResponse
+} from '../../codeyCommand';
+import { getEmojiByName } from '../../components/emojis';
+
+const MEMBER_API = 'https://csclub.uwaterloo.ca/api/members.json';
+
+interface memberStatus {
+  name: string;
+  id: string;
+  program: string;
+}
+
+type UwIdType = string | undefined;
+
+/*
+ * Get member embed
+ */
+const getMemberEmbed = async (uwid: UwIdType): Promise<MessageEmbed> => {
+  const title = 'CSC Membership Information';
+  if (!uwid) {
+    return new MessageEmbed().setColor('RED').setTitle(title).setDescription('Please provide a UW ID!');
+  }
+
+  const members = (await (await fetch(MEMBER_API)).json()).members as memberStatus[];
+  const foundMember = members.filter((m) => m.id == uwid).length > 0;
+
+  if (foundMember) {
+    return new MessageEmbed()
+      .setColor('GREEN')
+      .setTitle(title)
+      .setDescription(`You're a CSC member! Hooray! ${getEmojiByName('codeyLove')}`);
+  }
+
+  const NOT_MEMBER_DESCRIPTION = `You're not a CSC member! ${getEmojiByName('codeySad')}
+
+Being a CSC member comes with gaining access to CSC machines, cloud, email, web hosting, and more! Additional details can be found here! https://csclub.uwaterloo.ca/resources/services/
+
+To sign up, you can follow the instructions here! https://csclub.uwaterloo.ca/get-involved/`;
+  return new MessageEmbed().setColor('RED').setTitle(title).setDescription(NOT_MEMBER_DESCRIPTION);
+};
+
+const executeCommand: SapphireMessageExecuteType = async (
+  _client,
+  messageFromUser,
+  args,
+  _initialMessageFromBot
+): Promise<SapphireMessageResponse> => {
+  let uwId: UwIdType;
+  if (messageFromUser instanceof Message) {
+    const { content } = messageFromUser;
+    const messageArgs = content.split(' ').filter((m) => m != '.member');
+    if (messageArgs.length == 1) uwId = messageArgs[0];
+  } else if ('uwid' in args) {
+    uwId = args['uwid'] as string;
+  }
+  const memberEmbed = await getMemberEmbed(uwId);
+  return { embeds: [memberEmbed] };
+};
+
+export const memberCommandDetails: CodeyCommandDetails = {
+  name: 'member',
+  aliases: [],
+  description: 'Gets CSC membership information',
+  detailedDescription: `**Examples:**
+\`${container.botPrefix}member [id]\``,
+
+  isCommandResponseEphemeral: true,
+  messageWhenExecutingCommand: 'Getting CSC membership information...',
+  executeCommand: executeCommand,
+  messageIfFailure: 'Could not retrieve CSC membership information.',
+  codeyCommandResponseType: CodeyCommandResponseType.EMBED,
+
+  options: [
+    {
+      name: 'uwid',
+      description: 'Quest ID',
+      type: CodeyCommandOptionType.STRING,
+      required: true
+    }
+  ],
+  subcommandDetails: {}
+};
