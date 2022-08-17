700245e2190b09cedabd0bba2315361d9db9cf3d
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2022-08-16 21:27:20 -0400

    Move member logic to commandDetails

--- src/commands/miscellaneous/member.ts
+++ src/commands/miscellaneous/member.ts
@@ -7,46 +7,10 @@ import {
   SapphireMessageExecuteType,
   SapphireMessageResponse
 } from '../../codeyCommand';
+import { getMemberEmbed, UWIdType } from '../../commandDetails/miscellaneous/member';
 import { getEmojiByName } from '../../components/emojis';
-import { Message, MessageEmbed } from 'discord.js';
+import { Message } from 'discord.js';
 import fetch from 'node-fetch';
-
-const MEMBER_API = 'https://csclub.uwaterloo.ca/~a3thakra/csc/adi-member-json-api/api/members.json';
-
-interface memberStatus {
-  name: string;
-  id: string;
-  program: string;
-}
-
-type UWIdType = string | undefined;
-
-/*
- * Get member embed
- */
-export const getMemberEmbed = async (uwid: UWIdType): Promise<MessageEmbed> => {
-  const title = 'CSC Membership Information';
-  if (uwid === undefined) {
-    return new MessageEmbed().setColor('RED').setTitle(title).setDescription('Please provide a UW ID!');
-  }
-
-  const members = (await (await fetch(MEMBER_API)).json()).members as memberStatus[];
-  const foundMember = members.filter((m) => m.id == uwid).length > 0;
-
-  if (foundMember) {
-    return new MessageEmbed()
-      .setColor('GREEN')
-      .setTitle(title)
-      .setDescription(`You're a CSC member! Hooray! ${getEmojiByName('codeyLove')}`);
-  }
-
-  const NOT_MEMBER_DESCRIPTION = `You're not a CSC member! ${getEmojiByName('codeySad')}
-
-Being a CSC member comes with gaining access to CSC machines, cloud, email, web hosting, and more! Additional details can be found here https://csclub.uwaterloo.ca/resources/services/
-
-To sign up, you can follow the instructions here! https://csclub.uwaterloo.ca/get-involved/`;
-  return new MessageEmbed().setColor('RED').setTitle(title).setDescription(NOT_MEMBER_DESCRIPTION);
-};
 
 const executeCommand: SapphireMessageExecuteType = async (
   _client,
