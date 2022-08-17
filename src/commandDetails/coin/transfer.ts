69c48030ffccc374bed07f7a53c52b2c9b6f7694
botato <botatooo@protonmail.com>
2022-08-16 22:33:26 -0400

    feat(coin): implement transfer functionality

---
+++ src/commandDetails/coin/transfer.ts
@@ -0,0 +1,98 @@
+import { container } from '@sapphire/framework';
+import { User } from 'discord.js';
+import {
+  CodeyCommandDetails,
+  CodeyCommandOptionType,
+  CodeyCommandResponseType,
+  SapphireMessageExecuteType
+} from '../../codeyCommand';
+import { getCoinBalanceByUserId, changeDbCoinBalanceByUserId, UserCoinEvent } from '../../components/coin';
+import { getCoinEmoji, getEmojiByName } from '../../components/emojis';
+
+// Update coin balance of a user
+const coinUpdateExecuteCommand: SapphireMessageExecuteType = async (client, messageFromUser, args) => {
+  // First mandatory argument is user
+  const user = <User>args['user'];
+  if (!user) {
+    throw new Error('please enter a valid user mention or ID for balance adjustment.');
+  }
+
+  // Second mandatory argument is amount
+  const amount = args['amount'];
+  if (typeof amount !== 'number') {
+    throw new Error('please enter a valid amount to adjust.');
+  }
+
+  // Optional argument is reason
+  const reason = args['reason'] ?? null;
+
+  // Get balance
+  const senderBalance = await getCoinBalanceByUserId(messageFromUser.member!.user.id);
+  const recipientBalance = await getCoinBalanceByUserId(user.id);
+
+  // Implement some basic checks
+  if (messageFromUser.member!.user.id === user.id)
+    return `You can't transfer money to yourself ${getEmojiByName('codeyConfused')}.`;
+  if (senderBalance < amount)
+    return `You don't have enough coins to complete this transfer ${getEmojiByName('codeyConfused')}.`;
+  if (amount < 0) return `You can't steal from people ${getEmojiByName('codeyAngry')}.`;
+  if (amount === 0) return `What exactly are you trying to do there ${getEmojiByName('codeyStressed')}?`;
+
+  // Transfer coins
+  await changeDbCoinBalanceByUserId(
+    messageFromUser.member!.user.id,
+    senderBalance,
+    senderBalance - <number>amount,
+    UserCoinEvent.CoinTransferSender,
+    <string | null>reason
+  );
+  await changeDbCoinBalanceByUserId(
+    user.id,
+    recipientBalance,
+    recipientBalance + <number>amount,
+    UserCoinEvent.CoinTransferRecipient,
+    <string | null>reason
+  );
+
+  // Get new balance
+  const newRecipientBalance = await getCoinBalanceByUserId(user.id);
+
+  return `${user.username} now has ${newRecipientBalance} Codey coins ${getCoinEmoji()} ${getEmojiByName(
+    'codeyPoggers'
+  )}.`;
+};
+
+export const coinTransferCommandDetails: CodeyCommandDetails = {
+  name: 'transfer',
+  aliases: ['t'],
+  description: 'Transfer money to another user.',
+  detailedDescription: `**Examples:**
+  \`${container.botPrefix}coin transfer @Codey 100 Gave me a cookie.\``,
+
+  isCommandResponseEphemeral: false,
+  messageWhenExecutingCommand: 'Transfering coins...',
+  executeCommand: coinUpdateExecuteCommand,
+  codeyCommandResponseType: CodeyCommandResponseType.STRING,
+
+  options: [
+    {
+      name: 'user',
+      description: 'The user to transfer the amount to,',
+      type: CodeyCommandOptionType.USER,
+      required: true
+    },
+    {
+      name: 'amount',
+      description: 'The amount to transfer,',
+      type: CodeyCommandOptionType.NUMBER,
+      required: true
+    },
+    {
+      name: 'reason',
+      description: 'The reason why we are transfering money,',
+      type: CodeyCommandOptionType.STRING,
+      required: false
+    }
+  ],
+  subcommandDetails: {}
+};
