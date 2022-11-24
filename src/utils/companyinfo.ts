a9fd5b072d1d22ec6af0df093ac6c1e02f9e3afe
Victor Zheng <vzheng@gmail.com>
2022-11-24 16:40:56 -0500

    comments

--- src/utils/companyinfo.ts
+++ src/utils/companyinfo.ts
@@ -2,20 +2,23 @@ import { vars } from '../config';
 import { isValidUrl } from './validateUrl';
 import fetch from 'node-fetch';
 
+const CRUNCHBASE_ORGANIZATION_URL = 'crunchbase.com/organization/';
+const CRUNCHBASE_ORGANIZATION_API_URL = 'https://api.crunchbase.com/api/v4/entities/organizations/';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getCompanyInfo = async (companyCrunchbaseLink: string) => {
   // check if url contains crunchbase.com/organization
-  // if not, return null
+  // if not, throw error
   if (
     isValidUrl(companyCrunchbaseLink) &&
-    !companyCrunchbaseLink.includes('crunchbase.com/organization/')
+    !companyCrunchbaseLink.includes(CRUNCHBASE_ORGANIZATION_URL)
   ) {
     throw new Error('Invalid URL');
   }
 
   let companyName;
-  // check if urlPattern matches company_crunchbase_link
-  if (companyCrunchbaseLink.includes('crunchbase.com/organization/')) {
+  // check if urlPattern matches companyCrunchbaseLink
+  if (companyCrunchbaseLink.includes(CRUNCHBASE_ORGANIZATION_URL)) {
     // grab everything after the last / character
     companyName = companyCrunchbaseLink.split('/').pop();
   } else {
@@ -24,13 +27,12 @@ const getCompanyInfo = async (companyCrunchbaseLink: string) => {
   }
 
   const response = await fetch(
-    `https://api.crunchbase.com/api/v4/entities/organizations/${companyName}?user_key=${vars.CRUNCHBASE_API_KEY}&field_ids=categories,short_description`,
+    `${CRUNCHBASE_ORGANIZATION_API_URL}${companyName}?user_key=${vars.CRUNCHBASE_API_KEY}&field_ids=categories,short_description`,
   );
   const data = await response.json();
   if (data.error) {
     throw new Error(data.error);
   }
-  console.log(data);
 
   return data;
 };
