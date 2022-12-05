372553ab985b912fa85f143a67d8b52fa1580f5c
Ashish Selvaraj <52637731+SaurontheMighty@users.noreply.github.com>
2022-12-04 22:54:13 -0500

    minor update

--- src/utils/dm.ts
+++ src/utils/dm.ts
@@ -1,7 +1,7 @@
 import { User } from 'discord.js';
 import { logger } from '../logger/default';
 
-export const sendMessage = async (discordUser: User, message: string) => {
+export const sendMessage = async (discordUser: User, message: string): Promise<void> => {
   try {
     await discordUser.send(message);
   } catch (err) {
