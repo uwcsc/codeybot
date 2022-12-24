206d9b12fcb5d02feb0e838aced491fe833ba5b1
probro27 <prabhavkhera@hotmail.com>
2022-12-24 15:14:10 -0500

    Documentation for function added

--- src/components/db.ts
+++ src/components/db.ts
@@ -140,6 +140,16 @@ const initRpsGameInfo = async (db: Database): Promise<void> => {
   );
 };
 
+/*
+  function: addSQLColumnIfNotExists
+  parameters:
+    * db: the object of SQLite Database
+    * tableName: the name of the table where the column needs to be added
+    * columnName: the name of the column that needs to be added to the table
+    * columnDataType: the SQL data type of the column that needs to be added
+  Example:
+    addSQLColumnIfNotExists(db, 'user_profile_table', 'profile_emoji', 'VARCHAR(32)')
+*/
 const addSQLColumnIfNotExists = async (
   db: Database,
   tableName: string,
