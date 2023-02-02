e7be3b00102db7d49af1de1d0210a2816bbd6b67
Fan-Yang <fan.yang284@gmail.com>
2023-02-02 15:27:04 -0500

    setup base transfer command

---
+++ src/commandDetails/coin/transfer.ts
@@ -0,0 +1,48 @@
+import { container } from '@sapphire/framework';
+import { Permissions, User } from 'discord.js';
+import {
+  CodeyCommandOptionType,
+  CodeyCommandDetails,
+  SapphireMessageExecuteType,
+} from '../../codeyCommand';
+
+const coinTransferExecuteCommand: SapphireMessageExecuteType = async (
+  client,
+  messageFromUser,
+  args,
+) => {
+  return '';
+};
+
+export const coinTransferCommandDetails: CodeyCommandDetails = {
+  name: 'transfer',
+  aliases: ['t'],
+  description: 'Transfer coins to another user.',
+  detailedDescription: `**Examples:**
+	\`${container.botPrefix}coin transfer @Codey 10\``,
+
+  isCommandResponseEphemeral: false,
+  messageWhenExecutingCommand: 'Transferring coins...',
+  executeCommand: coinTransferExecuteCommand,
+  options: [
+    {
+      name: 'user',
+      description: 'The user to transfer coins to.',
+      type: CodeyCommandOptionType.USER,
+      required: true,
+    },
+    {
+      name: 'amount',
+      description: 'The amount to transfer to the specified user.',
+      type: CodeyCommandOptionType.NUMBER,
+      required: true,
+    },
+    {
+      name: 'reason',
+      description: 'The reason for transferring.',
+      type: CodeyCommandOptionType.STRING,
+      required: false,
+    },
+  ],
+  subcommandDetails: {},
+};
