41ca6e4d7190041eda3ad99ac14ef41bed6d0a07
Fan-Yang <fan.yang284@gmail.com>
2023-02-07 15:18:13 -0500

    add transfer functionality, comments

--- src/commandDetails/coin/transfer.ts
+++ src/commandDetails/coin/transfer.ts
@@ -1,10 +1,19 @@
 import { container } from '@sapphire/framework';
-import { Permissions, User } from 'discord.js';
+import { User } from 'discord.js';
 import {
   CodeyCommandOptionType,
   CodeyCommandDetails,
   SapphireMessageExecuteType,
+  getUserFromMessage,
+  SapphireMessageResponseWithMetadata,
 } from '../../codeyCommand';
+import {
+  getCoinBalanceByUserId,
+  updateCoinBalanceByUserId,
+  UserCoinEvent,
+} from '../../components/coin';
+import { getCoinEmoji } from '../../components/emojis';
+import { pluralize } from '../../utils/pluralize';
 
 const coinTransferExecuteCommand: SapphireMessageExecuteType = async (
   client,
@@ -18,7 +27,7 @@ const coinTransferExecuteCommand: SapphireMessageExecuteType = async (
   }
 
   // Second mandatory argument is amount
-  const amount = args['amount'];
+  const amount = <number>args['amount'];
   if (typeof amount !== 'number') {
     throw new Error('please enter a valid amount to transfer.');
   }
@@ -26,7 +35,32 @@ const coinTransferExecuteCommand: SapphireMessageExecuteType = async (
   // Optional argument is reason
   const reason = args['reason'];
 
-  return '';
+  // Retrieve the sender balance and ensure they have enough coins
+  const senderBalance = await getCoinBalanceByUserId(getUserFromMessage(messageFromUser).id);
+  if (amount > senderBalance) {
+    return new SapphireMessageResponseWithMetadata(
+      `You don't have enough ${getCoinEmoji()} to place that bet.`,
+      {},
+    );
+  }
+
+  // Retrieve the receiver balance, add the coins transferred and update
+  const receiverBalance = await getCoinBalanceByUserId(receivingUser.id);
+  await updateCoinBalanceByUserId(
+    receivingUser.id,
+    <number>(receiverBalance + amount),
+    UserCoinEvent.AdminCoinUpdate,
+    <string>(reason ?? ''),
+    client.user?.id,
+  );
+
+  // Get new balance
+  const newBalance = await getCoinBalanceByUserId(receivingUser.id);
+
+  return `${receivingUser.username} now has ${newBalance} Codey ${pluralize(
+    'coin',
+    newBalance,
+  )} ${getCoinEmoji()}.`;
 };
 
 export const coinTransferCommandDetails: CodeyCommandDetails = {
