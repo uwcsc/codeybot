ab3cad2791d05ee87ad4ede0886bc630d4fb11dd
mcpenguin <marcus.chanwc@gmail.com>
2023-03-03 19:45:15 -0500

    intermediate

--- src/components/leetcode.ts
+++ src/components/leetcode.ts
@@ -6,12 +6,92 @@ const leetcodeIdUrl = 'https://lcid.cc/info';
 const leetcodeApiUrl = 'https://leetcode.com/graphql';
 const leetcodeUrl = 'https://leetcode.com/problems';
 
+const LeetcodeTagsDict = {
+  array: 'Array',
+  string: 'String',
+  'hash-table': 'Hash Table',
+  'dynamic-programming': 'Dynamic Programming',
+  math: 'Math',
+  sorting: 'Sorting',
+  greedy: 'Greedy',
+  'depth-first-search': 'Depth-First Search',
+  database: 'Database',
+  'binary-search': 'Binary Search',
+  'breadth-first-search': 'Breadth-First Search',
+  tree: 'Tree',
+  matrix: 'Matrix',
+  'binary-tree': 'Binary Tree',
+  'two-pointers': 'Two Pointers',
+  'bit-manipulation': 'Bit Manipulation',
+  stack: 'Stack',
+  'heap-priority-queue': 'Heap (Priority Queue)',
+  graph: 'Graph',
+  design: 'Design',
+  'prefix-sum': 'Prefix Sum',
+  simulation: 'Simulation',
+  counting: 'Counting',
+  backtracking: 'Backtracking',
+  'sliding-window': 'Sliding Window',
+  'union-find': 'Union Find',
+  'linked-list': 'Linked List',
+  'ordered-set': 'Ordered Set',
+  'monotonic-stack': 'Monotonic Stack',
+  enumeration: 'Enumeration',
+  recursion: 'Recursion',
+  trie: 'Trie',
+  'divide-and-conquer': 'Divide and Conquer',
+  'binary-search-tree': 'Binary Search Tree',
+  queue: 'Queue',
+  bitmask: 'Bitmask',
+  memoization: 'Memoization',
+  geometry: 'Geometry',
+  'segment-tree': 'Segment Tree',
+  'topological-sort': 'Topological Sort',
+  'number-theory': 'Number Theory',
+  'binary-indexed-tree': 'Binary Indexed Tree',
+  'hash-function': 'Hash Function',
+  'game-theory': 'Game Theory',
+  combinatorics: 'Combinatorics',
+  'shortest-path': 'Shortest Path',
+  'data-stream': 'Data Stream',
+  interactive: 'Interactive',
+  'string-matching': 'String Matching',
+  'rolling-hash': 'Rolling Hash',
+  randomized: 'Randomized',
+  brainteaser: 'Brainteaser',
+  'monotonic-queue': 'Monotonic Queue',
+  'merge-sort': 'Merge Sort',
+  iterator: 'Iterator',
+  concurrency: 'Concurrency',
+  'doubly-linked-list': 'Doubly-Linked List',
+  'probability-and-statistics': 'Probability and Statistics',
+  quickselect: 'Quickselect',
+  'bucket-sort': 'Bucket Sort',
+  'suffix-array': 'Suffix Array',
+  'minimum-spanning-tree': 'Minimum Spanning Tree',
+  'counting-sort': 'Counting Sort',
+  shell: 'Shell',
+  'line-sweep': 'Line Sweep',
+  'reservoir-sampling': 'Reservoir Sampling',
+  'eulerian-circuit': 'Eulerian Circuit',
+  'radix-sort': 'Radix Sort',
+  'strongly-connected-component': 'Strongly Connected Component',
+  'rejection-sampling': 'Rejection Sampling',
+  'biconnected-component': 'Biconnected Component',
+};
+
 interface LeetcodeTopicTag {
   name: string;
 }
 
+enum LeetcodeDifficulty {
+  'Easy',
+  'Medium',
+  'Hard',
+}
+
 interface LeetcodeIdProblemDataFromUrl {
-  difficulty: 'Easy' | 'Medium' | 'Hard';
+  difficulty: LeetcodeDifficulty;
   likes: number;
   dislikes: number;
   categoryTitle: string;
@@ -33,7 +113,7 @@ interface LeetcodeProblemDataFromUrl {
 }
 
 interface LeetcodeProblemData {
-  difficulty: 'Easy' | 'Medium' | 'Hard';
+  difficulty: LeetcodeDifficulty;
   likes: number;
   dislikes: number;
   categoryTitle: string;
@@ -47,8 +127,10 @@ interface LeetcodeProblemData {
   problemId: number;
 }
 
-// I couldn't find a way to get this dynamically, so a constant suffices for now
-export const totalNumberOfLeetcodeProblems = 2577;
+interface LeetcodeProblemQuery {
+  difficulty: LeetcodeDifficulty;
+  tags: string;
+}
 
 export const getLeetcodeProblemDataFromId = async (
   problemId: number,
