b4502d948560c375c4af36651a367ac6909b89dd
Marcus Chan <76544623+mcpenguin@users.noreply.github.com>
2023-03-05 12:59:32 -0500

    removed console log and added linter rule (#458)
    * removed console log and added linter rule
    
    * removed unnecessary logger info

--- src/commandDetails/company/profile.ts
+++ src/commandDetails/company/profile.ts
@@ -14,7 +14,6 @@ const companyProfileExecuteCommand: SapphireMessageExecuteType = async (
   const user = getUserFromMessage(messageFromUser);
   const companies = await getCompaniesByUserId(user.id);
   // return a string where each line is the company_id - company_name
-  console.log(companies);
   const companyString = companies.map((company) => `${company.name} - ${company.role}`).join('\n');
   return `You are associated with the following companies:\n${companyString}`;
 };
