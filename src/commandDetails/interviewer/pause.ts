54dd05ea33793e2ef820b05b30622f3f22c17ed2
Di Nguyen <dingyen96@gmail.com>
2024-01-25 20:14:15 +0000

    Added file to manage pause subcommand

---
+++ src/commandDetails/interviewer/pause.ts
@@ -0,0 +1,49 @@
+import { CodeyUserError } from './../../codeyUserError';
+import { container } from '@sapphire/framework';
+import { Message } from 'discord.js';
+import _ from 'lodash';
+import { getInterviewer, pauseProfile } from '../../components/interviewer';
+import {
+    CodeyCommandDetails,
+    CodeyCommandOptionType,
+    SapphireMessageExecuteType,
+    SapphireMessageResponse,
+} from '../../codeyCommand';
+
+const pauseExecuteCommand: SapphireMessageExecuteType = async (
+    _client,
+    messageFromUser,
+    _args,
+): Promise<SapphireMessageResponse> => {
+    const message = <Message>messageFromUser;
+    const { id } = message.author;
+
+    // Check if user signed up to be interviewer
+    if (!(await getInterviewer(id))) {
+        throw new CodeyUserError(
+            message,
+            `You don't seem to have signed up yet, please sign up using \`${container.botPrefix}interviewer signup <calendarUrl>\`!`,
+        );
+    }
+
+    // Pause interviewer data
+    await pauseProfile(id);
+    return message.reply(
+        `Your interviewer profile has been paused! You will not appear in interviewer queries anymore, until you run \`${container.botPrefix}interviewer resume\`.`,
+    );
+};
+
+export const pauseCommandDetails: CodeyCommandDetails = {
+    name: 'pause',
+    aliases: [],
+    description: 'Placeholder',
+    detailedDescription: `**Examples:**
+\`${container.botPrefix}interviewer pause\``,
+
+    isCommandResponseEphemeral: false,
+    messageWhenExecutingCommand: 'Placeholder',
+    executeCommand: pauseExecuteCommand,
+    messageIfFailure: 'Placeholder',
+    options: [],
+    subcommandDetails: {},
+};
