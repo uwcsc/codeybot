dd68b82ccd88cf0e4e410db91a7bdf730ce7a49d
Di Nguyen <dingyen96@gmail.com>
2024-01-25 20:14:44 +0000

    Added file to manage resume subcommand

---
+++ src/commandDetails/interviewer/resume.ts
@@ -0,0 +1,47 @@
+import { CodeyUserError } from './../../codeyUserError';
+import { container } from '@sapphire/framework';
+import { Message } from 'discord.js';
+import _ from 'lodash';
+import { getInterviewer, resumeProfile } from '../../components/interviewer';
+import {
+    CodeyCommandDetails,
+    CodeyCommandOptionType,
+    SapphireMessageExecuteType,
+    SapphireMessageResponse,
+} from '../../codeyCommand';
+
+const resumeExecuteCommand: SapphireMessageExecuteType = async (
+    _client,
+    messageFromUser,
+    _args,
+): Promise<SapphireMessageResponse> => {
+    const message = <Message>messageFromUser;
+    const { id } = message.author;
+
+    // Check if user signed up to be interviewer
+    if (!(await getInterviewer(id))) {
+        throw new CodeyUserError(
+            message,
+            `You don't seem to have signed up yet. Please sign up using \`${container.botPrefix}interviewer signup <calendarUrl>\`!`,
+        );
+    }
+
+    // Resume interviewer data
+    await resumeProfile(id);
+    return message.reply('Your interviewer profile has been resumed!');
+};
+
+export const resumeCommandDetails: CodeyCommandDetails = {
+    name: 'resume',
+    aliases: [],
+    description: 'Placeholder',
+    detailedDescription: `**Examples:**
+\`${container.botPrefix}interviewer resume\``,
+
+    isCommandResponseEphemeral: false,
+    messageWhenExecutingCommand: 'Placeholder',
+    executeCommand: resumeExecuteCommand,
+    messageIfFailure: 'Placeholder',
+    options: [],
+    subcommandDetails: {},
+};
