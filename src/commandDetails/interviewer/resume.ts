1f8e284cb72b5d918f623ac9910bfeda764bdab3
Di Nguyen <dingyen96@gmail.com>
2024-01-27 04:42:27 +0000

    Refactored resume subcommand of interviewer

--- src/commandDetails/interviewer/resume.ts
+++ src/commandDetails/interviewer/resume.ts
@@ -1,13 +1,11 @@
-import { CodeyUserError } from './../../codeyUserError';
 import { container } from '@sapphire/framework';
-import { Message } from 'discord.js';
 import _ from 'lodash';
 import { getInterviewer, resumeProfile } from '../../components/interviewer';
 import {
     CodeyCommandDetails,
-    CodeyCommandOptionType,
     SapphireMessageExecuteType,
     SapphireMessageResponse,
+    getUserFromMessage,
 } from '../../codeyCommand';
 
 const interviewerResumeExecuteCommand: SapphireMessageExecuteType = async (
@@ -15,33 +13,29 @@ const interviewerResumeExecuteCommand: SapphireMessageExecuteType = async (
     messageFromUser,
     _args,
 ): Promise<SapphireMessageResponse> => {
-    const message = <Message>messageFromUser;
-    const { id } = message.author;
+    const id = getUserFromMessage(messageFromUser).id;
 
     // Check if user signed up to be interviewer
     if (!(await getInterviewer(id))) {
-        throw new CodeyUserError(
-            message,
-            `You don't seem to have signed up yet. Please sign up using \`${container.botPrefix}interviewer signup <calendarUrl>\`!`,
-        );
+        `You don't seem to have signed up yet. Please sign up using \`${container.botPrefix}interviewer signup <calendarUrl>\`!`;
     }
 
     // Resume interviewer data
     await resumeProfile(id);
-    return message.reply('Your interviewer profile has been resumed!');
+    return 'Your interviewer profile has been resumed!';
 };
 
 export const interviewerResumeCommandDetails: CodeyCommandDetails = {
     name: 'resume',
-    aliases: [],
-    description: 'Placeholder',
+    aliases: ['resume'],
+    description: 'Resume an interviewer profile',
     detailedDescription: `**Examples:**
 \`${container.botPrefix}interviewer resume\``,
 
     isCommandResponseEphemeral: false,
-    messageWhenExecutingCommand: 'Placeholder',
+    messageWhenExecutingCommand: 'Resuming profile...',
     executeCommand: interviewerResumeExecuteCommand,
-    messageIfFailure: 'Placeholder',
+    messageIfFailure: 'Could not resume profile',
     options: [],
     subcommandDetails: {},
 };
